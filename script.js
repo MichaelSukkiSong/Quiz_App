c204947575c57135a711e61b26e2f58ad156a1ab
sa77irak <sa77irak@gmail.com>
2020-11-22 00:27:45 +0900

    made state variables to keep track of questions so when clicked it could go to next question

--- script.js
+++ script.js
@@ -5,7 +5,9 @@
 - at the end show score
 ************************** */
 
-/* make data */
+var score, activeQuestion, answeringQuestions;
+
+/* make question data */
 
 // make question class
 class Question {
@@ -24,10 +26,18 @@ const question_3 = new Question("What's ur favorite programming language", [ 'ph
 // store them in an array
 const questions = [question_1, question_2, question_3];
 
+/* DOM elements */
+
+elements = {
+  question:document.querySelector(".question"),
+  options:document.querySelector(".options"),
+  form:document.querySelector(".question_form"),
+}
+
 /* functions */
 
-const displayQuestion = () => {
-  document.querySelector(".question").textContent = questions[0].question;
+const displayQuestion = (num) => {
+  elements.question.textContent = questions[num].question;
 }
 
 const displayOptions = (options) => {
@@ -35,21 +45,51 @@ const displayOptions = (options) => {
   for (let i = 0 ; i < options.length; i++) {
     const markup = `
     <div class="option">
-      <input type="radio" name="answer" id="option_${i+1}" ${i===0 ? "checked": ""}/>
-      <label for="option_${i+1}">${options[i]}</label>
+      <input type="radio" name="answer" id="option_${i}" value="answer_${i}" ${i===0 ? "checked": ""}/>
+      <label for="option_${i}">${options[i]}</label>
     </div>
     `;
     markup_sum = markup_sum + markup;
   }
-  document.querySelector(".options").insertAdjacentHTML("beforeend", markup_sum);
+  elements.options.insertAdjacentHTML("beforeend", markup_sum);
 }
 
-const startaQuestion = () => {
-  // display question and options
-  displayQuestion();
-  displayOptions(questions[1].options);
-  // get user choice
-  // compare user choice with answer and store score
+const compareAnswerAndUpdateScore = (questionAnswer, userAnswer) => {
+  if (questionAnswer === userAnswer) {
+    score++;
+    console.log(score)
+  }
 }
 
-startaQuestion();
+const nextQuestion = () => {
+  activeQuestion += 1;  
+  displayQuestion(activeQuestion);
+  displayOptions(questions[activeQuestion].options);
+}
+
+const init = () => {
+  activeQuestion = 0;
+  answeringQuestions = true;
+  displayQuestion(0);
+  displayOptions(questions[0].options);
+}
+
+/* button click */
+
+elements.form.addEventListener("submit", e => {
+  if (answeringQuestions) {
+    e.preventDefault();
+    var data = new FormData(elements.form);
+    const myChoice = Number(data.getAll('answer')[0].slice(-1))
+    console.log(myChoice)
+    if (myChoice) {
+      compareAnswerAndUpdateScore(questions[activeQuestion].answer, myChoice);
+      nextQuestion();
+    }
+  }
+}
+)
+
+init();
+
+// TODO: clear question function
