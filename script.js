4fb6d432b221b8a99f387a242107371548d49486
sa77irak <sa77irak@gmail.com>
2020-11-21 17:12:09 +0900

    styled with css

--- script.js
+++ script.js
@@ -4,3 +4,13 @@
 - select answer
 - at the end show score
 ************************** */
+
+class Question {
+  constructor(question, option, answer) {
+    this.question = question;
+    this.option = option;
+    this.answer = answer;
+  }
+}
+
+const question_1 = new Question("What r u gonaa learn after JS?", [1. React, 2. Node, 3. php, 4. python], 1);
